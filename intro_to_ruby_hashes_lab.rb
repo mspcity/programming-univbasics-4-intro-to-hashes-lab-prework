1df402c67c3930f82c3b2c44caad4c7336a82310
Youngmin Park <codepark2019@gmail.com>
2020-05-06 07:20:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,11 +5,12 @@ end
 def my_hash
   # return a valid hash with any key/value pair of your choice
   my_hash = {:a => 1}
-  
+
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pianeer = {:name => 'Grace Hopper'}
 end
 
 def id_generator
